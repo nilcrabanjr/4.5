16fb3127c4f452714d51bc67bfb42747fdb64449
Riley <104562698@student.swin.edu.au>
2025-04-26 17:20:21 +1000

    Expanded the createBarChart function to draw a basic chart from the loaded CSV data.

--- js/d3script.js
+++ js/d3script.js
@@ -3,21 +3,28 @@ const svg = d3.select(".responsive-svg-container")
     .attr("viewBox", "0 0 1200 1600")
     .style("border", "1px solid black");
 
-const createBarChart = (data) => {
-  svg.append("rect")
-    .attr("x", 10)
-    .attr("y", 10)
-    .attr("width", 414)
-    .attr("height", 16)
-    .attr("fill", "blue");
-};
+const createBarChart = data => {
+  const barHeight = 20;   
+  const barSpacing = 10;   
 
+  svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("class", d => `bar bar-${d.count}`)
+    .attr("x", 0)                   
+    .attr("y", (d, i) => i * (barHeight + barSpacing))
+    .attr("width", d => d.count)            
+    .attr("height", barHeight)       
+    .attr("fill", "blue");  
+};
 d3.csv("data/task4data.csv", d => {
   return {
     brand: d.brand,
     count: +d.count
   };
 }).then(data => {
+  data.sort((a, b) => d3.descending(a.count, b.count));
   console.log(data);
   console.log(data.length);
   console.log(d3.max(data, d => d.count));
